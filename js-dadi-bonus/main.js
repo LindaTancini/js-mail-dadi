aedc1a11ba951ab518001cf95db33cf1f893036e
LindaTancini <lindatancini95@gmail.com>
2025-02-27 16:18:26 +0100

    feat: add bonus dadi

---
+++ js-dadi-bonus/main.js
@@ -0,0 +1,20 @@
+// genero un numero random da 1 a 6 per l'utente con prompt (BONUS)
+let userScore = parseInt(
+  prompt("E' il tuo turno di lanciare il dado! Inserisci un numero da 1 a 6")
+);
+console.log(userScore);
+// genero un numero random da 1 a 6 per il pc
+let computerScore = Math.floor(Math.random() * 6) + 1;
+console.log(computerScore);
+
+// stabilisco chi è il vincitore del tiro con i dadi in base al numero più alto
+// se user è più alto di pc allora vince
+if (userScore > computerScore) {
+  console.log("L'utente ha vinto!");
+  //altrimenti se pc è più alto di user allora vince
+} else if (computerScore > userScore) {
+  console.log("Il computer ha vinto!");
+  //E se è un pareggio?
+} else {
+  console.log("Pareggio!");
+}
