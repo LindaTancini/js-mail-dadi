b7f5e598648005b76347dc85521e3cd89134a258
LindaTancini <lindatancini95@gmail.com>
2025-02-27 15:52:04 +0100

    feat: add userScore with math.floor

--- js-dadi/main.js
+++ js-dadi/main.js
@@ -1,1 +1,8 @@
-console.log("Linda");
+// genero un numero random da 1 a 6 per l'utente
+let userScore = Math.floor(Math.random() * 6) + 1;
+console.log(userScore);
+// genero un numero random da 1 a 6 per il pc
+
+//stampo i risultati dei tiri dei dadi
+
+// il vincitore è il numero di tiro del dado più alto
