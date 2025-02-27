239c9ddfad03a6ddc93219b84fdb8fce3ea49e5f
LindaTancini <lindatancini95@gmail.com>
2025-02-27 14:25:24 +0100

    feat: create array with email

--- js-mail/main.js
+++ js-mail/main.js
@@ -1,5 +1,11 @@
 // creo una lista di email (array)
-
+const email = [
+  "linda@gmail.com",
+  "luigi@gmail.com",
+  "leone@gmail.com",
+  "fumetto@gmail.com",
+];
+console.log(email);
 // chiedo all'utente la sua email
 
 // la tua email è presente nella lista?
